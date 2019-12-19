3bf77980439494f54b1fef6ff4e747f79ec699ab
arfa <arfa@example.com>
2019-12-19 18:21:39 +0700

    Node JS

--- Handling data request/server.js
+++ Handling data request/server.js
@@ -1,23 +1,27 @@
 var http = require("http");
 var fs = require("fs");
+//include fungsi-fungsi yang ada pada module url
 var url = require("url");
+//include fungsi-fungsi yang ada pada module querystring
+var qString = require("querystring");
 
 http.createServer(function(req, res){
     if (req.url != "/favicon.ico") {
+
+        //parse url : memecah url menjadi beberapa bagian object
+        //dan mengambil object queryString
+        //req.url : mengambil request url dari clien
         var access = url.parse(req.url);
-        var file = "";
         if (access.pathname == "/") {
-            file = "./index.html";
-        } else if (access.pathname == "/contact") {
-            file = "./contact.html"
-        } else {
-            file = "./404.html";
+            
+            //parse query : memecah querystring menjadi beberapa bagian object(JSON)
+            var data = qString.parse(access.query);
+            res.writeHead(200, {"Content-Type" : "text/plain"});
+            res.end(JSON.stringify(data));
         }
-        res.writeHead(200,{"Content-Type" : "text/html"});
-        // membaca file pada halaman conten
-        fs.createReadStream(file).pipe(res);
+
     }
     
-}).listen(8888);
+}).listen(3000);
 
 console.log("Server is running!");
